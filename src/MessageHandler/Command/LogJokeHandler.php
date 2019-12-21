f604c6b3ce4c8eeb7e5039ecb8126b59e07399f9
Egor Ushakov <eushakov@gmail.com>
2019-12-21 17:46:20 +0300

    Add checking exception is thrown on invalid LogJoke command

--- src/MessageHandler/Command/LogJokeHandler.php
+++ src/MessageHandler/Command/LogJokeHandler.php
@@ -4,9 +4,11 @@
 namespace App\MessageHandler\Command;
 
 
+use App\Exception\Message\Command\LogJokeException;
 use App\Message\Command\LogJoke;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class LogJokeHandler implements MessageHandlerInterface
 {
@@ -14,18 +16,28 @@ class LogJokeHandler implements MessageHandlerInterface
      * @var LoggerInterface
      */
     private $logger;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
 
     /**
      * LogJokeHandler constructor.
      * @param LoggerInterface $logger
+     * @param ValidatorInterface $validator
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, ValidatorInterface $validator)
     {
         $this->logger = $logger;
+        $this->validator = $validator;
     }
 
     public function __invoke(LogJoke $command)
     {
+        $errors = $this->validator->validate($command);
+        if (count($errors) > 0) {
+            throw new LogJokeException((string )$errors);
+        }
         $this->logger->info($command->getJoke());
     }
 
--- tests/Unit/LogJokeHandlerTest.php
+++ tests/Unit/LogJokeHandlerTest.php
@@ -2,33 +2,53 @@
 
 namespace App\Tests\Unit;
 
+use App\Exception\Message\Command\LogJokeException;
 use App\Message\Command\LogJoke;
 use App\MessageHandler\Command\LogJokeHandler;
 use Generator;
-use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-class LogJokeHandlerTest extends TestCase
+class LogJokeHandlerTest extends CustomTestCase
 {
     /**
-     * @param LogJoke $command
-     * @dataProvider getLogJokes
+     * @testdox Check the joke is logged properly on valid command
      */
-    public function testJokeIsLogged(LogJoke $command): void
+    public function testJokeIsLogged(): void
     {
+        $command = new LogJoke('email1@example.com', 'Mwa-ha-ha!');
         $logger = $this->createMock(LoggerInterface::class);
         $logger->expects($this->once())
             ->method('info')
             ->with($command->getJoke());
-        $handler = new LogJokeHandler($logger);
+        $handler = new LogJokeHandler($logger, $this->getValidatorWithErrorsCount(0));
         $this->assertInstanceOf(MessageHandlerInterface::class, $handler);
         $handler($command);
     }
 
-    public function getLogJokes(): Generator
+    public function getInvalidLogJokes(): Generator
     {
-        yield [new LogJoke('email1@example.com', 'Mwa-ha-ha!')];
-        yield [new LogJoke('email2@example.com', 'WFT!')];
+        yield [new LogJoke('email1@example.com', ''), 1];
+        yield [new LogJoke('email2example.com', 'WFT!'), 1];
+        yield [new LogJoke('email2example.com', ''), 2];
     }
+
+    /**
+     * @param LogJoke $command
+     * @param int $errorsCount
+     * @dataProvider getInvalidLogJokes
+     */
+    public function testExceptionIsThrownOnInvalidCommand(LogJoke $command, int $errorsCount): void
+    {
+        $this->expectException(LogJokeException::class);
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger
+            ->method('info')
+            ->with($command->getJoke());
+        $handler = new LogJokeHandler($logger, $this->getValidatorWithErrorsCount($errorsCount));
+        $this->assertInstanceOf(MessageHandlerInterface::class, $handler);
+        $handler($command);
+    }
+
+
 }
